f4fd40749b825bc80f5c3de4721719d65011e238
Dima <fosubly2001@gmail.com>
2026-02-17 19:52:37 +0300

    fix2

--- games/find_diff/find_diff.js
+++ games/find_diff/find_diff.js
@@ -4,7 +4,7 @@ const Canvas = document.querySelector("#find_diff_canvas")
 function initCanvas() {
     console.log(Canvas);
 
-    let img = Canvas.querySelector(".game_area__left_side img").src = "/games/find_diff/assets/first/right.png"
+    let img = Canvas.querySelector(".game_area__left_side img").src = "./games/find_diff/assets/first/right.png"
     console.log(img);
 
 }
